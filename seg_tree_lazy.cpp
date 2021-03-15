f440f6232852f6b02113014d1abecdf53de55498
Arjun31415 <arjunp0710@gmail.com>
2021-03-15 19:19:52 +0530

    Update seg_tree_lazy.cpp
    Changed the code completely
    supports both range update, point update, etc.
    Currently working for range max query

--- seg_tree_lazy.cpp
+++ seg_tree_lazy.cpp
@@ -1,9 +1,10 @@
 /* 
     Segment tree supporting point updates and range queries 
-    source array (a) is 0 indexed 
-    segment tree (t) is 1 indexed 
-    call modify function for range updates.
-    modify fuunction also supports point updates by giving range as[l,l] 
+   currently supports range max query 
+   TODO: range sum queries
+   current lazy update is set a[i]+=val;
+   version 2 will have
+   set a[i]=x;
 */
 
 #include <bits\stdc++.h>
@@ -19,92 +20,151 @@ using namespace std;
     cin.tie(0);                   \
     cout.tie(0)
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-struct SegmentTree
+struct Data
 {
-    int n;
-    vector<int> a, t, lazy;
+    //Use required attributes
+    int mn = 0;
 
-    void init(int x)
-    {
-        n = x;
-        t.assign(4 * n, 0);
-        lazy.assign(4 * n, 0);
-    }
-
-    void init(int x, vector<int> y)
-    {
-        n = x;
-        a = y;
-        t.resize(4 * n);
-        lazy.assign(4 * n, 0);
-        build(1, 0, n - 1);
-    }
-
-    int merge(int x, int y) { return x + y; }
-
-    int upd(int x, int y) { return x + y; }
-
-    void build(int i, int l, int r)
-    {
-        if (l == r)
-        {
-            t[i] = a[l];
-            return;
-        }
-        int mid = (l + r) / 2;
-        build(lc(i), l, mid);
-        build(rc(i), mid + 1, r);
-        t[i] = merge(t[lc(i)], t[rc(i)]);
-    }
-
-    void push(int i, int l, int r) // i-> tree index , l-> left bound, r-> right bound for range updates
-    {
-        t[i] = upd(t[i], (lazy[i]));
-        if (l != r)
-        {
-            lazy[lc(i)] = upd(lazy[lc(i)], lazy[i]);
-            lazy[rc(i)] = upd(lazy[rc(i)], lazy[i]);
-        }
-        lazy[i] = 0;
-    }
-    // call modify not push
-    void modify(int i, int l, int r, int ql, int qr, int val)
-    {
-        if (l > qr || r < ql)
-            return;
-        if (lazy[i] != 0)
-            push(i, l, r);
-        if (l >= ql && r <= qr)
-        {
-            lazy[i] = upd(lazy[i], val);
-            push(i, l, r);
-            return;
-        }
-        int mid = (l + r) / 2;
-        modify(lc(i), l, mid, ql, qr, val);
-        modify(rc(i), mid + 1, r, ql, qr, val);
-        t[i] = merge(t[lc(i)], t[rc(i)]);
-    }
-
-    int query(int i, int l, int r, int ql, int qr)
-    {
-        if (l > qr || r < ql)
-            return 0;
-        if (lazy[i] != 0)
-            push(i, l, r);
-        if (l >= ql && r <= qr)
-            return t[i];
-        int mid = (l + r) / 2;
-        int resL = query(lc(i), l, mid, ql, qr);
-        int resR = query(rc(i), mid + 1, r, ql, qr);
-        return merge(resL, resR);
-    }
-
-    void modify(int l, int r, int val) { modify(1, 0, n - 1, l, r, val); }
-
-    void modify(int pos, int val) { modify(1, 0, n - 1, pos, pos, val); }
-
-    int query(int l, int r) { return query(1, 0, n - 1, l, r); }
-
-    int query(int pos) { return query(1, 0, n - 1, pos, pos); }
+    //Default Values
+    Data() : mn(0){};
 };
+
+struct SegTree
+{
+    int N;
+    vector<Data> st;
+    vector<bool> cLazy;
+    vector<int> lazy;
+
+    void init(int n)
+    {
+        N = n;
+        st.resize(3 * N + 5);
+        cLazy.assign(3 * N + 5, false);
+        lazy.assign(3 * N + 5, 0);
+    }
+
+    //Write reqd merge functions
+    void merge(Data &cur, Data &l, Data &r)
+    {
+        cur.mn = max(l.mn, r.mn);
+    }
+
+    //Handle lazy propagation appriopriately
+    void propagate(int node, int L, int R)
+    {
+        if (lazy[node] == 0)
+            return;
+        st[node].mn += lazy[node];
+        if (L != R)
+        {
+            cLazy[node * 2] = 1;
+            cLazy[node * 2 + 1] = 1;
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+
+        cLazy[node] = 0;
+        lazy[node] = 0;
+    }
+
+    void build(int node, int L, int R)
+    {
+        if (L == R)
+        {
+            st[node].mn = 0;
+            return;
+        }
+        int M = L + (R - L) / 2;
+        build(node * 2, L, M);
+        build(node * 2 + 1, M + 1, R);
+        merge(st[node], st[node * 2], st[node * 2 + 1]);
+    }
+
+    Data Query(int node, int L, int R, int i, int j)
+    {
+        if (cLazy[node])
+            propagate(node, L, R);
+        if (j < L || i > R)
+            return Data();
+        if (i <= L && R <= j)
+            return st[node];
+        int M = (L + R) / 2;
+        Data left = Query(node * 2, L, M, i, j);
+        Data right = Query(node * 2 + 1, M + 1, R, i, j);
+        Data cur;
+        merge(cur, left, right);
+        return cur;
+    }
+
+    Data pQuery(int node, int L, int R, int pos)
+    {
+        if (cLazy[node])
+            propagate(node, L, R);
+        if (L == R)
+            return st[node];
+        int M = L + (R - L) / 2;
+        if (pos <= M)
+            return pQuery(node * 2, L, M, pos);
+        else
+            return pQuery(node * 2 + 1, M + 1, R, pos);
+    }
+
+    void Update(int node, int L, int R, int i, int j, int val)
+    {
+        if (cLazy[node])
+            propagate(node, L, R);
+        if (j < L || i > R)
+            return;
+        if (i <= L && R <= j)
+        {
+            cLazy[node] = 1;
+            lazy[node] += val;
+            propagate(node, L, R);
+            return;
+        }
+        int M = L + (R - L) / 2;
+        Update(node * 2, L, M, i, j, val);
+        Update(node * 2 + 1, M + 1, R, i, j, val);
+        merge(st[node], st[node * 2], st[node * 2 + 1]);
+    }
+
+    void pUpdate(int node, int L, int R, int pos, int val)
+    {
+        if (cLazy[node])
+            propagate(node, L, R);
+        if (L == R)
+        {
+            cLazy[node] = 1;
+            lazy[node] = val;
+            propagate(node, L, R);
+            return;
+        }
+        int M = L + (R - L) / 2;
+        if (pos <= M)
+            pUpdate(node * 2, L, M, pos, val);
+        else
+            pUpdate(node * 2 + 1, M + 1, R, pos, val);
+        merge(st[node], st[node * 2], st[node * 2 + 1]);
+    }
+
+    Data query(int pos)
+    {
+        return pQuery(1, 1, N, pos);
+    }
+
+    Data query(int l, int r)
+    {
+        return Query(1, 1, N, l, r);
+    }
+
+    void update(int pos, int val)
+    {
+        pUpdate(1, 1, N, pos, val);
+    }
+
+    void update(int l, int r, int val)
+    {
+        Update(1, 1, N, l, r, val);
+    }
+} ST;
