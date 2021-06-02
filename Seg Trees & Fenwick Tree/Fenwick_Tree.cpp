ad021b5a810dcdfe38fd89e3f36455613a406e21
Arjun31415 <arjunp0710@gmail.com>
2021-06-02 11:20:17 +0530

    bug fixes and made it template

--- Seg Trees & Fenwick Tree/Fenwick_Tree.cpp
+++ Seg Trees & Fenwick Tree/Fenwick_Tree.cpp
@@ -8,23 +8,30 @@ typedef long long ll;
 // Range updates and point queries;
 // to update a range [a,b] with +x; do update(a,x) and update(1+b,-x);
 // to query a range [0,a] do ft.query(a+1);
+
+template <typename T>
 struct FT
 {
-    vector<ll> s;
-    FT(int n) : s(n) {}
-    void update(int pos, ll dif)
+    vector<T> s;
+    FT(long long n) : s(n) {}
+    void update(unsigned long long pos, T dif)
     {
         // a[pos] += dif
         for (; pos < sz(s); pos |= pos + 1)
             s[pos] += dif;
     }
     // returns sum of values in [0, pos)
-    ll query(int pos)
+    T query(unsigned long long pos)
     {
 
         ll res = 0;
         for (; pos > 0; pos &= pos - 1)
             res += s[pos - 1];
         return res;
+    }
+    //  query [l,r];
+    T query(unsigned long long l, unsigned long long r)
+    {
+        query(r + 1) - query(l);
     }
 };
