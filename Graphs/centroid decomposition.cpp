1e46a1b85d42685d7a61d5d688679f307ae8c832
Arjun31415 <arjunp0710@gmail.com>
2021-06-02 11:21:38 +0530

    Centroid decomposition

--- Graphs/centroid decomposition.cpp
+++ Graphs/centroid decomposition.cpp
@@ -125,6 +125,6 @@ int main()
     {
         int x, y;
         scanf("%d %d", &x, &y);
-        printf("%d\n", compute_distance(x + 1, y + 1));
+        printf("%d\n", compute_distance(x, y));
     }
 }
