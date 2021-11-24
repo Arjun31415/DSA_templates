c8c19865e7caffa38aa71554c6db666f2b35347e
Arjun31415 <arjunp0710@gmail.com>
2021-11-24 21:57:55 +0530

    Few Updates

--- Graphs/Floyd-Warshall.cpp
+++ Graphs/Floyd-Warshall.cpp
@@ -18,12 +18,12 @@ typedef long long ll;
 vector<vi> edges, d;
 int n, m;
 
-/**
+/**e
 
  * @brief finds all pair shortest path between the nodes present in the graph.
  * If the two nodes are not reachable then d[i][j]=inf and if there is a negative cycle between them
  * d[i][j] = -inf
- * @param edges edges is the edge list stored as {u,v,w}. i.e efge from u->v woth weight w
+ * @param edges edges is the edge list stored as {u,v,w}. i.e edge from u->v with weight w
  * @param n n is number of vertices
  * @param m m is the number of edges;
  * @param d the matrix in which the distance is to be stored
@@ -40,6 +40,7 @@ void floyd_Warshall(vector<vi> edges, int n, int m, vector<vi> &d = ::d)
         d[e[0]][e[1]] = min(d[e[0]][e[1]], e[2]);
     }
     for (int k = 1; k <= n; ++k)
+    {
         for (int i = 1; i <= n; i++)
             for (int j = 1; j <= n; j++)
             {
@@ -48,6 +49,13 @@ void floyd_Warshall(vector<vi> edges, int n, int m, vector<vi> &d = ::d)
                     d[i][j] = min(d[i][j], d[k][j] + d[i][k]);
                 }
             }
+        for (int i = 1; i <= n; i++, cout << "\n")
+            for (int j = 1; j <= n; j++)
+            {
+                cout << (d[i][j] == inf ? -1 : d[i][j]) << " ";
+            }
+        cout << "\n\n";
+    }
     // if a path from i to j has a negative cycle: d[i][j] =-inf
 
     for (int i = 1; i <= n; i++)
@@ -56,7 +64,7 @@ void floyd_Warshall(vector<vi> edges, int n, int m, vector<vi> &d = ::d)
             for (int x = 1; x <= n; x++)
             {
                 // is th possible to reach j from i through x
-                //and is the distance from x to itself less than 0
+                // and is the distance from x to itself less than 0
                 if (d[i][x] < inf and d[x][j] < inf and d[x][x] < 0)
                 {
                     d[i][j] = -inf;
@@ -69,5 +77,14 @@ void floyd_Warshall(vector<vi> edges, int n, int m, vector<vi> &d = ::d)
 }
 signed main()
 {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.pb({u, v, w});
+        edges.pb({v, u, w});
+    }
+    floyd_Warshall(edges, n, m);
     return 0;
 }
